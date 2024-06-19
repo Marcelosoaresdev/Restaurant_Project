34638bfd6e4749add02bb0889f54e09073153d32
Marcelo Soares <marcelohsoares142@gmail.com>
2024-06-19 15:50:37 -0300

    jquery tel mask plugin and jquery validate

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('#telefone').mask('(00) 0000-0000');
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            mensagem: {
+                required: true
+            }
+        }
+    })
+})
